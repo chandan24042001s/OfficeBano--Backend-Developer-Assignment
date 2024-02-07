08eaa05a67d7b1770d918301b31024574e4614d9
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2024-02-07 16:30:56 +0530

    Delete 05 - Dummy-MERN directory

--- 05 - Dummy-MERN/backend/controller/getUsers.js
+++
@@ -1,9 +0,0 @@
-const User = require("../models/User");
-exports.getUser = async (req, res) => {
-	try {
-		const userData = await User.find({});
-		res.json({ success: true, data: userData });
-	} catch (error) {
-		res.status(500).json({ success: false, error: error });
-	}
-};
--- 05 - Dummy-MERN/backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createUser } = require("../controller/createUser");
-const { getUser } = require("../controller/getUsers");
-router.post("/createUser", createUser);
-router.get("/getallUsers", getUser);
-
-module.exports = router;
